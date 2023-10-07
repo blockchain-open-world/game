6d819b5ae1750c0eb4b296bff96d66348110fa21
Satoshi Disciple <discipleofsatoshi@gmail.com>
2023-10-07 20:43:15 -0300

    actions

--- constants.gd
+++ constants.gd
@@ -1,7 +1,7 @@
 class_name Contants
 
-#const websocket_url = "wss://node1.blockchainopenworld.com/connect"
-#const server_url = "https://node1.blockchainopenworld.com/%s"
+const websocket_url = "wss://node1.blockchainopenworld.com/connect"
+const server_url = "https://node1.blockchainopenworld.com/%s"
 
-const websocket_url = "ws://127.0.0.1:8082/connect"
-const server_url = "http://127.0.0.1:8082/%s"
+#const websocket_url = "ws://127.0.0.1:8082/connect"
+#const server_url = "http://127.0.0.1:8082/%s"
